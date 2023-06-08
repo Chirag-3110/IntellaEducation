13cb736f33d25d684c85296905177a6cd6d62047
Chirag <takchirag828@gmail.com>
2023-06-08 22:01:50 +0530

    some changes and one teacher card changed

--- js/index.js
+++ js/index.js
@@ -1,39 +1,46 @@
 const teachers=[
     {
-        name:"Chirag tak",
-        image:"images/teachers/teacher-1.jpg",
-        subjects:["Computer Networking","Computer Security","Human Computer"],
-        expertize:"Computer Science"
+        name:"Jason Fernandes",
+        image:"assets/teachers/teacher1.jpeg",
+        subjects:["Crypto","Tokenomics",],
+        expertize:"Chief Bussiness Officer NFT Tech",
+        experience:"10 years of Experience"
     },
     {
         name:"John Wick",
         image:"images/teachers/teacher-1.jpg",
         subjects:["Computer Networking","Computer Security","Human Computer Interfacing"],
-        expertize:"History"
+        expertize:"History",
+        experience:"10 years of Experience"
+        
     },
     {
         name:"Tom Elish",
         image:"images/teachers/teacher-1.jpg",
         subjects:["Computer Networking","Computer Security","Human Computer Interfacing"],
-        expertize:"Computer Science"
+        expertize:"Computer Science",
+        experience:"10 years of Experience"
     },
     {
         name:"John Doe",
         image:"images/teachers/teacher-1.jpg",
         subjects:["Computer Networking","Computer Security","Human Computer Interfacing"],
-        expertize:"Computer Science"
+        expertize:"Computer Science",
+        experience:"10 years of Experience"
     },
     {
         name:"Tom Holland",
         image:"images/teachers/teacher-1.jpg",
         subjects:["Computer Networking","Computer Security","Human Computer Interfacing"],
-        expertize:"Spider Man"
+        expertize:"Spider Man",
+        experience:"10 years of Experience"
     },
     {
         name:"Tony Stark",
         image:"images/teachers/teacher-1.jpg",
         subjects:["Computer Networking","Human Computer Interfacing"],
-        expertize:"Iron Man"
+        expertize:"Iron Man",
+        experience:"10 years of Experience"
     },
 ]
 
@@ -50,7 +57,7 @@ const showModal=(tecaherObj)=>{
     modalWrapper=document.createElement("div");
     modalWrapper.innerHTML=`
         <div class="modal fade" id="teachermodal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-dialog" role="document">
             <div  class="modal-content rounded-0 border-0 p-4">
                 <div class="modal-header border-0">
                     <h3>Teacher Details</h3>
@@ -61,21 +68,20 @@ const showModal=(tecaherObj)=>{
                 <div class="modal-body">
                     <div style="display: flex;flex-direction: column;align-items: center;" >
                     <img style="width:150px;border-radius:50%" src=${teacher.image} alt="teacher">
-                    <div class="col-md-12 mb-5">
+                    <div class="col-md-12 mb-5" style="margin-top:25px;" >
                         <div >
                             <h3>${teacher.name}</h3>
                             <h6 class="text-color" >${teacher.expertize}</h6>
+                            <h6 class="text-color" >${teacher.experience}</h6>
                         </div>
-                        <div >
-                            <h4 class="mb-4" style="margin-top:15px" >SUMMARY OF ACTIVITIES/INTERESTS</h4>
-                            <ul class="list-unstyled row">
+                            <h4 class="mb-4" style="margin-top:15px;" >Subjects</h4>
+                            <ul class="list-unstyled row" style="margin-top:-15px;" >
                                 ${
                                     teacher.subjects.map((item)=>{
-                                        return `<li class="mb-3"  style="margin-left:15px">${item}</li>`
+                                        return `<li  style="margin-left:15px;">${item}</li>`
                                     })
                                 }
                             </ul>
-                        </div>
                     </div>
                     </div>
                 </div>
